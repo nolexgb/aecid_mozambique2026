dca9912ab7428b4e2269560b5749d65632056ef6
Nolan Gómez <nolan.gb83@gmail.com>
2026-06-26 13:04:00 +0200

    Add project data for various regions in Mozambique

---
+++ data.js
@@ -0,0 +1,254 @@
+const projectData = [
+  {
+    name: "Ámbito Nacional",
+    lat: -18.6657,
+    lng: 35.5296,
+    projects: [
+      {
+        title: "Proyecto sanitario para garantizar la atención a pacientes con VIH avanzado en el CRAM Alto Maé, Maputo",
+        year: "2024",
+        partners: "CRAM Alto Maé / MISAU",
+        amount: "480.000 €",
+        modality: "Bilateral sectorial"
+      },
+      {
+        title: "Apoyo a las capacidades de investigación en el sector de la salud en Mozambique",
+        year: "2024",
+        partners: "Fundação Manhiça",
+        amount: "1.500.000 €",
+        modality: "Bilateral"
+      },
+      {
+        title: "Programa para el refuerzo de capacidades profesionales del sector marítimo-pesquero de Mozambique",
+        year: "2024",
+        partners: "AMARC",
+        amount: "146.000 €",
+        modality: "Bilateral"
+      },
+      {
+        title: "Fondo Común PROSAÚDE III",
+        year: "2024",
+        partners: "MISAU",
+        amount: "500.000 €",
+        modality: "Bilateral sectorial"
+      },
+      {
+        title: "Fortalecimiento de capacidades del Sistema Nacional de Salud mozambiqueño para formación de médicos especialistas",
+        year: "2024",
+        partners: "ISCISA",
+        amount: "500.000 €",
+        modality: "Bilateral sectorial"
+      },
+      {
+        title: "Refuerzo del Sistema Nacional de Salud mozambiqueño frente a la crisis sanitaria COVID-19",
+        year: "2022",
+        partners: "MISAU",
+        amount: "500.000 €",
+        modality: "Bilateral"
+      },
+      {
+        title: "Apoyo al fortalecimiento de capacidades de investigación en salud en Mozambique",
+        year: "2022",
+        partners: "CISM",
+        amount: "1.500.000 €",
+        modality: "Bilateral"
+      },
+      {
+        title: "Viabilizar el impacto de la investigación clínica en la calidad de la asistencia sanitaria",
+        year: "2025",
+        partners: "INS",
+        amount: "700.000 €",
+        modality: "Bilateral"
+      },
+      {
+        title: "Refuerzo de capacidades del sistema público mozambiqueño de enseñanza técnico profesional en TIC e innovación",
+        year: "2025",
+        partners: "ANEP",
+        amount: "300.000 €",
+        modality: "Bilateral"
+      }
+    ]
+  },
+  {
+    name: "Gaza",
+    lat: -23.0222,
+    lng: 32.7181,
+    projects: [
+      {
+        title: "Fortalecer y diversificar medios de vida sostenibles de cinco comunidades del distrito de Chicualacuala",
+        year: "2021",
+        partners: "CIC-Batá",
+        amount: "500.693 €",
+        modality: "Convocatoria ONGD"
+      },
+      {
+        title: "Promoción del acceso de la juventud a la formación profesional con enfoque de género",
+        year: "2024",
+        partners: "Madre Coraje / Asociación Kulima",
+        amount: "600.000 €",
+        modality: "Convocatoria ONGD"
+      },
+      {
+        title: "Mejora del acceso y calidad de la formación profesional de colectivos vulnerables de cuatro distritos de Gaza",
+        year: "2023",
+        partners: "Madre Coraje",
+        amount: "800.000 €",
+        modality: "Convocatoria ONGD"
+      },
+      {
+        title: "Fomento de los derechos económicos de mujeres supervivientes de violencia de género en Gaza",
+        year: "2023",
+        partners: "FEPF / AIPCC",
+        amount: "573.332 €",
+        modality: "Convocatoria ONGD"
+      }
+    ]
+  },
+  {
+    name: "Maputo",
+    lat: -25.9692,
+    lng: 32.5732,
+    projects: [
+      {
+        title: "Recuperación y conservación del manglar en el Municipio de Maputo",
+        year: "2022",
+        partners: "Municipio de Maputo",
+        amount: "476.100 €",
+        modality: "Bilateral sectorial"
+      },
+      {
+        title: "Proyecto de apoyo para la evaluación de la colección fílmica del Instituto Nacional de las Industrias Culturales y Creativas",
+        year: "2025",
+        partners: "INICC",
+        amount: "123.192,50 €",
+        modality: "Bilateral cultural"
+      },
+      {
+        title: "Innovación y desarrollo de la cadena de valor de la moringa para la agricultura familiar comunitaria",
+        year: "2021",
+        partners: "Enraíza",
+        amount: "623.656 €",
+        modality: "Innovación"
+      },
+      {
+        title: "Mejora de la seguridad alimentaria y nutrición en población vulnerable de Magude",
+        year: "2025",
+        partners: "ONGAWA",
+        amount: "900.798 €",
+        modality: "Convocatoria ONGD"
+      },
+      {
+        title: "HabitAR 6: contribuir al derecho a la ciudad con énfasis en mujeres e infancia",
+        year: "2024",
+        partners: "ASF",
+        amount: "1.000.000 €",
+        modality: "Convocatoria ONGD"
+      }
+    ]
+  },
+  {
+    name: "Inhambane",
+    lat: -23.865,
+    lng: 35.3833,
+    projects: [
+      {
+        title: "Formación profesional de calidad para la inserción laboral de la juventud más vulnerable",
+        year: "2022",
+        partners: "Jóvenes y Desarrollo",
+        amount: "3.000.000 €",
+        modality: "Convenio"
+      },
+      {
+        title: "Fomento de vida libre de violencia contra mujeres mediante refuerzo de liderazgo y autonomía económica",
+        year: "2023",
+        partners: "APS",
+        amount: "356.000 €",
+        modality: "Convocatoria ONGD"
+      }
+    ]
+  },
+  {
+    name: "Zambézia",
+    lat: -16.5639,
+    lng: 36.6094,
+    projects: [
+      {
+        title: "Promoción de la economía circular basada en el bambú para creación de empleo y adaptación climática en Etiopia y Mozambique",
+        year: "2025",
+        partners: "INBAR",
+        amount: "790.000 €",
+        modality: "Bilateral sectorial"
+      }
+    ]
+  },
+  {
+    name: "Cabo Delgado",
+    lat: -12.3335,
+    lng: 39.3206,
+    projects: [
+      {
+        title: "Apoyo a la capacitación para la gobernanza descentralizada e inclusiva en Cabo Delgado",
+        year: "2023",
+        partners: "SEPF de Cabo Delgado",
+        amount: "800.000 €",
+        modality: "Bilateral"
+      },
+      {
+        title: "Fortalecimiento de la participación de las mujeres en espacios de decisión política",
+        year: "2021",
+        partners: "SEPF / GDE",
+        amount: "100.000 €",
+        modality: "Bilateral"
+      },
+      {
+        title: "Fortalecimiento de RRHH del sector salud de Cabo Delgado mediante formación técnica",
+        year: "2024",
+        partners: "SEPF / CPD",
+        amount: "214.950 €",
+        modality: "Bilateral"
+      },
+      {
+        title: "Mejora de la calidad, cobertura y resiliencia del sistema de salud de Cabo Delgado",
+        year: "2022",
+        partners: "FAMME",
+        amount: "1.300.000 €",
+        modality: "Convenio"
+      }
+    ]
+  },
+  {
+    name: "Niassa y Cabo Delgado",
+    lat: -13.5,
+    lng: 37.8,
+    projects: [
+      {
+        title: "Fase III: mejora de la seguridad alimentaria de familias de Niassa y Cabo Delgado",
+        year: "2025",
+        partners: "Mundukide",
+        amount: "600.000 €",
+        modality: "Convocatoria ONGD"
+      },
+      {
+        title: "Mejorar la seguridad alimentaria de campesinos de Niassa y Cabo Delgado",
+        year: "2023",
+        partners: "Mundukide",
+        amount: "534.932 €",
+        modality: "Convocatoria ONGD"
+      }
+    ]
+  },
+  {
+    name: "ONGD Región Norte y Sur",
+    lat: -19.5,
+    lng: 33.5,
+    projects: [
+      {
+        title: "Sistemas de teledetección agroclimática para una agricultura resiliente y de precisión en Mozambique",
+        year: "2024",
+        partners: "CESAL / Universidad de Córdoba",
+        amount: "600.000 €",
+        modality: "Innovación"
+      }
+    ]
+  }
+];
