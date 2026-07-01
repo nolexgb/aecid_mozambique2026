fd303beab832835b8ca73c907ecbb73255a625ca
Nolan Gómez <nolan.gb83@gmail.com>
2026-07-01 16:03:52 +0200

    Create dictionary.js

---
+++ data/dictionary.js
@@ -0,0 +1,117 @@
+/* ==========================================================
+   DICCIONARIOS
+========================================================== */
+
+export const MODALITIES = {
+
+  Bilateral:{
+
+    color:"#003B79",
+
+    icon:"bilateral.svg"
+
+  },
+
+  "Convocatoria ONGD":{
+
+    color:"#F97316",
+
+    icon:"ongd.svg"
+
+  },
+
+  Convenio:{
+
+    color:"#16A34A",
+
+    icon:"convenio.svg"
+
+  },
+
+  Innovación:{
+
+    color:"#8E24AA",
+
+    icon:"innovacion.svg"
+
+  }
+
+};
+
+
+
+export const SECTORS={
+
+  Salud:"health",
+
+  Educación:"education",
+
+  Agricultura:"agriculture",
+
+  Agua:"water",
+
+  Gobernanza:"governance",
+
+  Género:"gender",
+
+  Medio Ambiente:"environment",
+
+  Formación Profesional:"tvet",
+
+  Pesca:"fisheries",
+
+  Cultura:"culture"
+
+};
+
+
+
+export const ODS={
+
+  1:"Fin de la pobreza",
+
+  2:"Hambre cero",
+
+  3:"Salud y bienestar",
+
+  4:"Educación de calidad",
+
+  5:"Igualdad de género",
+
+  6:"Agua limpia",
+
+  7:"Energía",
+
+  8:"Trabajo decente",
+
+  9:"Industria e innovación",
+
+  10:"Reducción desigualdades",
+
+  11:"Ciudades sostenibles",
+
+  12:"Producción responsable",
+
+  13:"Acción climática",
+
+  14:"Vida submarina",
+
+  15:"Vida terrestre",
+
+  16:"Paz y justicia",
+
+  17:"Alianzas"
+
+};
+
+
+
+export const STATUS={
+
+  Activo:"#16A34A",
+
+  Finalizado:"#6B7280",
+
+  Formulación:"#F59E0B"
+
+};
